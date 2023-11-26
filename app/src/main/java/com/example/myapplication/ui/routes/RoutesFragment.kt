c4a51ae1aa153f38be040989ca787e40c1be9895
Joycemcool <joyceding007@gmail.com>
2023-11-26 00:06:08 -0400

    Finished autocomplete

--- app/src/main/java/com/example/myapplication/ui/routes/RoutesFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/routes/RoutesFragment.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication.ui.routes
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentRoutesBinding
+
 
 class RoutesFragment : Fragment() {
 
@@ -24,9 +28,17 @@ class RoutesFragment : Fragment() {
         _binding = FragmentRoutesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        //
+        //Populate autocompleteTextView
+        //
+        val autoTransitNum = binding.autoCompleteTextViewSearch
+        val transitRoutesArray : Array<String> = resources.getStringArray((R.array.transit_numbers_array))
+        Log.i("ReadFromString",transitRoutesArray.toString())
 
-
-
+        val adapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,transitRoutesArray)
+        autoTransitNum.setAdapter(adapter)
+        autoTransitNum.threshold=1
+       
         binding.textDashboard.text ="This is the Routes fragment"
         return root
     }
