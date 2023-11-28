6ad6f0d3eada16c92c4670a807633c33c8dc6243
Joycemcool <joyceding007@gmail.com>
2023-11-27 20:23:11 -0400

    Read Internal Storage

--- app/src/main/java/com/example/myapplication/ui/routes/RoutesFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/routes/RoutesFragment.kt
@@ -1,15 +1,16 @@
 package com.example.myapplication.ui.routes
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentRoutesBinding
+import java.io.File
 
 
 class RoutesFragment : Fragment() {
@@ -38,6 +39,18 @@ class RoutesFragment : Fragment() {
         autoTransitNum.setAdapter(adapter)
         autoTransitNum.threshold=1
 
+
+        //Check Internal Storage exist
+        val filename = "myRoute"
+        val fileContents : List<String>
+        if(fileExist(filename)){
+            fileContents= context?.openFileInput(filename)?.bufferedReader()?.useLines { lines ->
+                lines.map {it.toString() }
+                    .filterNotNull()
+                    .toList()
+            } ?: emptyList()
+        }
+
         //
         //SetOnItemClickListener for autocompleteTextView
         //
@@ -45,17 +58,36 @@ class RoutesFragment : Fragment() {
            // var routeNumber : String;
             routeNumber=(adapter.getItem(position)?:"").toString()
             binding.textViewMyroutes.text = routeNumber
+            if(routeNumber!=null){
+
+
+                val fileContents = routeNumber
+
+
+                context?.openFileOutput(filename, Context.MODE_PRIVATE).use {
+                    it?.write(fileContents?.toByteArray())
+                }
+            }
+
         }
 
 
 
 
-        binding.textDashboard.text ="This is the Routes fragment"
+       // binding.textDashboard.text ="This is the Routes fragment"
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun fileExist(fname: String?): Boolean {
+        val file: File? = context?.getFileStreamPath(fname)
+        if (file != null) {
+            return file.exists()
+        }
+        else return false;
     }
 }
